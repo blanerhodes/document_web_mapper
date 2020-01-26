731c6e517f45a4d06d9908ae05f8328be47468fc
blanerhodes <43589468+blanerhodes@users.noreply.github.com>
2020-01-26 13:44:18 -0600

    Update createInput.sh

--- createInput.sh
+++ createInput.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#First arg: path to where file should be created (including file name)
+#Second arg options: 
+# ins (will open newly made file in vim)
+# view (will print the file contents to the terminal and not open in vim)
+# nv or no arg (will give a sucessfully created message but doesn't show file contents)
 touch "$1"
 xclip -o > "$1"
 ARG2=${2:-nv}
