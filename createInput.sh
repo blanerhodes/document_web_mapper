c362857930655fc80a60eb22a07824f34347a671
blanerhodes <blanecrhodes@gmail.com>
2020-01-26 13:36:43 -0600

    Added ins, view, nv, and default args to createInput.sh

--- createInput.sh
+++ createInput.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 touch "$1"
 xclip -o > "$1"
-vim "$1"
+ARG2=${2:-nv}
+if [ $ARG2 = 'ins' ]
+then
+	vim "$1"
+elif [ $ARG2 = 'view' ]
+then
+	less -FX "$1"
+elif [ $ARG2 = 'nv' ]
+then
+	echo Input file created.
+else
+	echo Unknown second arguement.	
+fi
