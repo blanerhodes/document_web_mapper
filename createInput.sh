ac8c43acbfc1c045b0082a71f52ca2b8cf91b814
blanerhodes <43589468+blanerhodes@users.noreply.github.com>
2020-02-11 20:47:32 -0600

    Update createInput.sh

--- createInput.sh
+++ createInput.sh
@@ -4,6 +4,8 @@
 # ins (will open newly made file in vim)
 # view (will print the file contents to the terminal and not open in vim)
 # nv or no arg (will give a sucessfully created message but doesn't show file contents)
+# put this script in $HOME/bin directory to use it from anywhere
+# you can alias a long path name by adding yourVar="/long/absolute/path" to ~/.bashrc and accessing it with $yourVar in terminal
 touch "$1"
 xclip -o > "$1"
 ARG2=${2:-nv}
